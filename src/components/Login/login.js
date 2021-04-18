67dce579eacc137ce8ab8eb5b9425a56ce0170f9
moquito64 <moquito64@gmail.com>
2021-04-17 16:18:53 -1000

    Added box to home with color changes and added login.css changes that are based on formik frameworks

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,43 +1,52 @@
-import React, { useState } from 'react';
- 
-function Login(props) {
-  const username = useFormInput('');
-  const password = useFormInput('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
- 
-  // handle button click of login form
-  const handleLogin = () => {
-    props.history.push('/dashboard');
-  }
- 
-  return (
-    <div>
-      Login<br /><br />
-      <div>
-        Username<br />
-        <input type="text" {...username} autoComplete="new-password" />
-      </div>
-      <div style={{ marginTop: 10 }}>
-        Password<br />
-        <input type="password" {...password} autoComplete="new-password" />
-      </div>
-      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
-      <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
-    </div>
-  );
-}
- 
-const useFormInput = initialValue => {
-  const [value, setValue] = useState(initialValue);
- 
-  const handleChange = e => {
-    setValue(e.target.value);
-  }
-  return {
-    value,
-    onChange: handleChange
-  }
-}
+import React from 'react';
+import { Formik } from 'formik';
+import './login.css'
+const Login = () => (
+<div>
+  <h1> Howl Login </h1>
+  <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <label className="login-form-labels">Username(email):</label>
+           <input type="email" name="email" onChange={handleChange} onBlur={handleBlur} value={values.email}/>
+           {errors.email && touched.email && errors.email}
+           <label className="login-form-labels">Password:</label>
+           <input type="password" name="password" onChange={handleChange} onBlur={handleBlur} value={values.password}/>
+           {errors.password && touched.password && errors.password}
+           <div className="spacer"></div>
+           <button type="submit" disabled={isSubmitting}>Login</button>
+         </form>
+       )}
+     </Formik>
+</div>
+);
  
 export default Login;
